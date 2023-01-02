6d0f2ceaa6c5d2cf5b0b57b631307ec2294310fa
unknown <92045666+man0lix@users.noreply.github.com>
2023-01-02 16:56:00 -0600

    added animefenix monoschinos and partial work cinehdplus

--- CineHDPlusProvider/src/main/kotlin/com/stormunblessed/CineHDPlusProvider.kt
+++ CineHDPlusProvider/src/main/kotlin/com/stormunblessed/CineHDPlusProvider.kt
@@ -1,16 +1,13 @@
 package com.lagradost.cloudstream3.movieproviders
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
-import com.lagradost.cloudstream3.mvvm.logError
-import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
+import org.jsoup.nodes.Element
 
-class Cuevana3Provider : MainAPI() {
-    override var mainUrl = "https://cuevana3.ai"
-    override var name = "Cuevana3"
+class CineHDPlusProvider:MainAPI() {
+    override var mainUrl = "https://cinehdplus.org"
+    override var name = "CineHDPlus"
     override var lang = "es"
     override val hasMainPage = true
     override val hasChromecastSupport = true
@@ -19,20 +16,19 @@ class Cuevana3Provider : MainAPI() {
         TvType.Movie,
         TvType.TvSeries,
     )
-
     override suspend fun getMainPage(page: Int, request : MainPageRequest): HomePageResponse {
         val items = ArrayList<HomePageList>()
         val urls = listOf(
-            Pair("$mainUrl/inicio/", "Recientemente actualizadas"),
-            Pair("$mainUrl/estrenos/", "Estrenos"),
+            Pair("$mainUrl/peliculas/", "Peliculas"),
+            // Pair("$mainUrl/estrenos/", "Estrenos"),
         )
         items.add(
             HomePageList(
                 "Series",
-                app.get("$mainUrl/serie", timeout = 120).document.select("section.home-series li")
+                app.get("$mainUrl/series/", timeout = 120).document.select("div.card")
                     .map {
-                        val title = it.selectFirst("h2.Title")!!.text()
-                        val poster = it.selectFirst("img.lazy")!!.attr("data-src")
+                        val title = it.selectFirst("div.card__content h3.a")!!.text()
+                        val poster = it.selectFirst("div.card__cover img.lazy")!!.attr("data-src")
                         val url = it.selectFirst("a")!!.attr("href")
                         TvSeriesSearchResponse(
                             title,
@@ -47,8 +43,8 @@ class Cuevana3Provider : MainAPI() {
         )
         urls.apmap { (url, name) ->
             val soup = app.get(url).document
-            val home = soup.select("section li.xxx.TPostMv").map {
-                val title = it.selectFirst("h2.Title")!!.text()
+            val home = soup.select("div.card").map {
+                val title = it.selectFirst("div.card__content h3.a")!!.text()
                 val link = it.selectFirst("a")!!.attr("href")
                 TvSeriesSearchResponse(
                     title,
@@ -67,18 +63,53 @@ class Cuevana3Provider : MainAPI() {
         if (items.size <= 0) throw ErrorLoadingException()
         return HomePageResponse(items)
     }
+    // private fun Element.toSearchResult(): SearchResponse {
+    //     val title = this.select(".listing-content p").text()
+    //     val href = this.select("a").attr("href")
+    //     val posterUrl = fixUrl(this.select(".Posters-img").attr("src"))
+    //     val isMovie = href.contains("/pelicula/")
+    //     return if (isMovie) {
+    //         MovieSearchResponse(
+    //             title,
+    //             href,
+    //             name,
+    //             TvType.Movie,
+    //             posterUrl,
+    //             null
+    //         )
+    //     } else {
+    //         TvSeriesSearchResponse(
+    //             title,
+    //             href,
+    //             name,
+    //             TvType.Movie,
+    //             posterUrl,
+    //             null,
+    //             null
+    //         )
+    //     }
+    // }
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val url = "$mainUrl/search/${query}"
+        val url = "$mainUrl/?s=${query}"
         val document = app.get(url).document
 
-        return document.select("li.xxx.TPostMv").map {
-            val title = it.selectFirst("h2.Title")!!.text()
+        return document.select("div.card").map {
+            val title = it.selectFirst("div.card__content h3.a")!!.text()
             val href = it.selectFirst("a")!!.attr("href")
-            val image = it.selectFirst("img.lazy")!!.attr("data-src")
-            val isSerie = href.contains("/serie/")
+            val image = it.selectFirst("div.card__cover img.lazy")!!.attr("data-src")
+            val isMovie = href.contains("/ver-pelicula/")
 
-            if (isSerie) {
+            if (isMovie) {
+                MovieSearchResponse(
+                    title,
+                    href,
+                    this.name,
+                    TvType.Movie,
+                    image,
+                    null
+                )
+            } else {
                 TvSeriesSearchResponse(
                     title,
                     href,
@@ -88,144 +119,84 @@ class Cuevana3Provider : MainAPI() {
                     null,
                     null
                 )
-            } else {
-                MovieSearchResponse(
-                    title,
-                    href,
-                    this.name,
-                    TvType.Movie,
-                    image,
-                    null
-                )
             }
         }
     }
 
     override suspend fun load(url: String): LoadResponse? {
-        val soup = app.get(url, timeout = 120).document
-        val title = soup.selectFirst("h1.Title")!!.text()
-        val description = soup.selectFirst(".Description p")?.text()?.trim()
-        val poster: String? = soup.selectFirst(".movtv-info div.Image img")!!.attr("data-src")
-        val year1 = soup.selectFirst("footer p.meta").toString()
-        val yearRegex = Regex("<span>(\\d+)</span>")
-        val yearf =
-            yearRegex.find(year1)?.destructured?.component1()?.replace(Regex("<span>|</span>"), "")
-        val year = if (yearf.isNullOrBlank()) null else yearf.toIntOrNull()
-        val episodes = soup.select(".all-episodes li.TPostMv article").map { li ->
-            val href = li.select("a").attr("href")
-            val epThumb =
-                li.selectFirst("div.Image img")?.attr("data-src") ?: li.selectFirst("img.lazy")!!
-                    .attr("data-srcc")
-            val seasonid = li.selectFirst("span.Year")!!.text().let { str ->
-                str.split("x").mapNotNull { subStr -> subStr.toIntOrNull() }
-            }
-            val isValid = seasonid.size == 2
-            val episode = if (isValid) seasonid.getOrNull(1) else null
-            val season = if (isValid) seasonid.getOrNull(0) else null
+        val soup = app.get(url).document
+
+        val title = soup.selectFirst("div.details__title h1")?.text()
+        val description = soup.selectFirst("div.mCSB_container mCS_y_hidden mCS_no_scrollbar_y p")?.text()?.trim()
+        val poster: String? = soup.selectFirst(".lazyloaded")?.attr("src")
+        val episodes = soup.select("div.tab-pane .btn").map { li ->
+            val href = li.selectFirst("a")?.attr("href")
+            val name = li.selectFirst(".btn-primary.btn-block")?.text()?.replace(Regex("(T(\\d+).*E(\\d+):)"),"")?.trim()
+            val seasoninfo = href?.substringAfter("temporada/")?.replace("/capitulo/","-")
+            val seasonid =
+                seasoninfo.let { str ->
+                    str?.split("-")?.mapNotNull { subStr -> subStr.toIntOrNull() }
+                }
+            val isValid = seasonid?.size == 2
+            val episode = if (isValid) seasonid?.getOrNull(1) else null
+            val season = if (isValid) seasonid?.getOrNull(0) else null
             Episode(
-                href,
-                null,
+                href!!,
+                name,
                 season,
                 episode,
-                fixUrl(epThumb)
             )
         }
-        val tags = soup.select("ul.InfoList li.AAIco-adjust:contains(Genero) a").map { it.text() }
-        val tvType = if (episodes.isEmpty()) TvType.Movie else TvType.TvSeries
-        val recelement =
-            if (tvType == TvType.TvSeries) "main section div.series_listado.series div.xxx"
-            else "main section ul.MovieList li"
-        val recommendations =
-            soup.select(recelement).mapNotNull { element ->
-                val recTitle = element.select("h2.Title").text() ?: return@mapNotNull null
-                val image = element.select("figure img")?.attr("data-src")
-                val recUrl = fixUrl(element.select("a").attr("href"))
-                MovieSearchResponse(
-                    recTitle,
-                    recUrl,
-                    this.name,
-                    TvType.Movie,
-                    image,
-                    year = null
-                )
-            }
-        val trailer = soup.selectFirst("div.TPlayer.embed_div div[id=OptY] iframe")?.attr("data-src")
 
+        val year = soup.selectFirst("div.sub-meta dateCreated")?.text()?.toIntOrNull()
+        val tvType = if (url.contains("/ver-pelicula/")) TvType.Movie else TvType.TvSeries
+        val tags = soup.select(".p-h-15.text-center a span.font-size-18.text-info.text-semibold")
+            .map { it?.text()?.trim().toString().replace(", ","") }
 
         return when (tvType) {
             TvType.TvSeries -> {
-                newTvSeriesLoadResponse(title,
-                    url, tvType, episodes,){
-                    this.posterUrl = poster
-                    this.plot = description
-                    this.year = year
-                    this.tags = tags
-                    this.recommendations = recommendations
-                }
+                TvSeriesLoadResponse(
+                    title!!,
+                    url,
+                    this.name,
+                    tvType,
+                    episodes,
+                    fixUrl(poster!!),
+                    year,
+                    description,
+                    null,
+                    null,
+                    tags,
+                )
             }
             TvType.Movie -> {
-                newMovieLoadResponse(title, url, tvType, url){
-                    this.posterUrl = poster
-                    this.plot = description
-                    this.year = year
-                    this.tags = tags
-                    this.recommendations = recommendations
-                    if (trailer!!.isNotBlank()) addTrailer(trailer)
-                }
-
+                MovieLoadResponse(
+                    title!!,
+                    url,
+                    this.name,
+                    tvType,
+                    url,
+                    fixUrl(poster!!),
+                    year,
+                    description,
+                    null,
+                    tags,
+                )
             }
             else -> null
         }
     }
-
-    data class Femcuevana(
-        @JsonProperty("url") val url: String,
-    )
-
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        app.get(data).document.select("div.TPlayerTb.embed_div iframe").apmap {
+        app.get(data).document.select("div.MTPlayerTb iframe").apmap {
             val iframe = fixUrl(it.attr("data-src"))
-            if (iframe.contains("api.cuevana3.me/fembed/")) {
-                val femregex =
-                    Regex("(https.\\/\\/api\\.cuevana3\\.me\\/fembed\\/\\?h=[a-zA-Z0-9]{0,8}[a-zA-Z0-9_-]+)")
-                femregex.findAll(iframe).map { femreg ->
-                    femreg.value
-                }.toList().apmap { fem ->
-                    val key = fem.replace("https://api.cuevana3.me/fembed/?h=", "")
-                    val url = app.post(
-                        "https://api.cuevana3.me/fembed/api.php",
-                        allowRedirects = false,
-                        headers = mapOf(
-                            "Host" to "api.cuevana3.me",
-                            "User-Agent" to USER_AGENT,
-                            "Accept" to "application/json, text/javascript, */*; q=0.01",
-                            "Accept-Language" to "en-US,en;q=0.5",
-                            "Content-Type" to "application/x-www-form-urlencoded; charset=UTF-8",
-                            "X-Requested-With" to "XMLHttpRequest",
-                            "Origin" to "https://api.cuevana3.me",
-                            "DNT" to "1",
-                            "Connection" to "keep-alive",
-                            "Sec-Fetch-Dest" to "empty",
-                            "Sec-Fetch-Mode" to "cors",
-                            "Sec-Fetch-Site" to "same-origin",
-                        ),
-                        data = mapOf(Pair("h", key))
-                    ).text
-                    val json = parseJson<Femcuevana>(url)
-                    val link = json.url
-                    if (link.contains("fembed")) {
-                        loadExtractor(link, data, subtitleCallback, callback)
-                    }
-                }
-            }
-            if (iframe.contains("tomatomatela")) {
+            if (iframe.contains("api.cinehdplus/")) {
                 val tomatoRegex =
-                    Regex("(\\/\\/apialfa.tomatomatela.club\\/ir\\/player.php\\?h=[a-zA-Z0-9]{0,8}[a-zA-Z0-9_-]+)")
+                    Regex("(\\/\\/api.cineplushd.org\\/ir\\/player.php\\?h=[a-zA-Z0-9]{0,8}[a-zA-Z0-9_-]+)")
                 tomatoRegex.findAll(iframe).map { tomreg ->
                     tomreg.value
                 }.toList().apmap { tom ->
@@ -250,15 +221,15 @@ class Cuevana3Provider : MainAPI() {
                     ).okhttpResponse.headers.values("location").apmap { loc ->
                         if (loc.contains("goto_ddh.php")) {
                             val gotoregex =
-                                Regex("(\\/\\/api.cuevana3.me\\/ir\\/goto_ddh.php\\?h=[a-zA-Z0-9]{0,8}[a-zA-Z0-9_-]+)")
+                                Regex("(\\/\\/api.cineplushd.org\\/ir\\/goto_ddh.php\\?h=[a-zA-Z0-9]{0,8}[a-zA-Z0-9_-]+)")
                             gotoregex.findAll(loc).map { goreg ->
-                                goreg.value.replace("//api.cuevana3.me/ir/goto_ddh.php?h=", "")
+                                goreg.value.replace("//api.cineplushd.org/ir/goto_ddh.php?h=", "")
                             }.toList().apmap { gotolink ->
                                 app.post(
-                                    "https://api.cuevana3.me/ir/redirect_ddh.php",
+                                    "https://api.cineplushd.org/ir/redirect_ddh.php",
                                     allowRedirects = false,
                                     headers = mapOf(
-                                        "Host" to "api.cuevana3.me",
+                                        "Host" to "api.cineplushd.org",
                                         "User-Agent" to USER_AGENT,
                                         "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
                                         "Accept-Language" to "en-US,en;q=0.5",
@@ -279,14 +250,14 @@ class Cuevana3Provider : MainAPI() {
                         }
                         if (loc.contains("index.php?h=")) {
                             val indexRegex =
-                                Regex("(\\/\\/api.cuevana3.me\\/sc\\/index.php\\?h=[a-zA-Z0-9]{0,8}[a-zA-Z0-9_-]+)")
+                                Regex("(\\/\\/api.cineplushd.org\\/sc\\/index.php\\?h=[a-zA-Z0-9]{0,8}[a-zA-Z0-9_-]+)")
                             indexRegex.findAll(loc).map { indreg ->
-                                indreg.value.replace("//api.cuevana3.me/sc/index.php?h=", "")
+                                indreg.value.replace("//api.cineplushd.org/sc/index.php?h=", "")
                             }.toList().apmap { inlink ->
                                 app.post(
-                                    "https://api.cuevana3.me/sc/r.php", allowRedirects = false,
+                                    "https://api.cineplushd.org/sc/r.php", allowRedirects = false,
                                     headers = mapOf(
-                                        "Host" to "api.cuevana3.me",
+                                        "Host" to "api.cineplushd.org",
                                         "User-Agent" to USER_AGENT,
                                         "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
                                         "Accept-Language" to "en-US,en;q=0.5",
